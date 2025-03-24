a7c963a42d41e983f30931e0aa38f0d9cddf1d1e
jeet-bheriya <jeetbheriya@gmail.com>
2025-03-24 15:52:33 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import AppProvider from "./context/AppProvider.tsx"
-import "@client/src/styles/global.css"
+import "@src/styles/global.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     // <React.StrictMode>
